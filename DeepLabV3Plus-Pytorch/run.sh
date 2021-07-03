d3e8a7b2d23b5efbf33ac168a71bfce756d531d2
beom <qjadud1994@gmail.com>
2021-07-03 03:45:32 +0000

    update DeepLabV3 script

--- DeepLabV3Plus-Pytorch/run.sh
+++ DeepLabV3Plus-Pytorch/run.sh
@@ -6,8 +6,8 @@ LR=0.05
 
 
 # training with 2 GPUs
-CUDA_VISLBLE_DEVICES=0,1 python main.py --data_root ${ROOT} --model ${MODEL} --gpu_id 0,1 --amp --total_itrs ${ITER} --batch_size ${BATCH} --lr ${LR}
+CUDA_VISLBLE_DEVICES=0,1 python main.py --data_root ${ROOT} --model ${MODEL} --gpu_id 0,1 --amp --total_itrs ${ITER} --batch_size ${BATCH} --lr ${LR}  --crop_val
 
 
 # evalutation with crf
-CUDA_VISIBLE_DEVICES=0,1 python eval.py --gpu_id 0,1 --data_root ${ROOT} --model ${MODEL}  --val_batch_size 16  --ckpt checkpoints/best_${MODEL}_voc_os16.pth
+CUDA_VISIBLE_DEVICES=0,1 python eval.py --gpu_id 0,1 --data_root ${ROOT} --model ${MODEL}  --val_batch_size 16  --ckpt checkpoints/best_${MODEL}_voc_os16.pth  --crop_val
